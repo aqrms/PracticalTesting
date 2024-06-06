b7d95e6fa62f59510920160cd8308a05b135de96
aqrms <aquarimus@gmail.com>
2024-06-06 20:17:31 +0900

    TODO: Sec5-3

---
+++ src/main/java/com/example/practicaltest/spring/domain/product/Product.java
@@ -0,0 +1,34 @@
+package com.example.practicaltest.spring.domain.product;
+
+import com.example.practicaltest.spring.domain.BaseEntity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Product extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String productNumber;
+
+    @Enumerated(EnumType.STRING)
+    private ProductType type;
+
+    @Enumerated(EnumType.STRING)
+    private ProductSellingType sellingType;
+
+    private String name;
+    private int price;
+}
