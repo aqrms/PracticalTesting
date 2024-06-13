077e0f1efd5d63e58ce60b8c4901e9cbc402fd02
aqrms <aquarimus@gmail.com>
2024-06-13 23:55:14 +0900

    Add Order Entities

---
+++ src/main/java/com/example/practicaltest/spring/domain/orderproduct/OrderProduct.java
@@ -0,0 +1,40 @@
+package com.example.practicaltest.spring.domain.orderproduct;
+
+import com.example.practicaltest.spring.domain.order.Order;
+import com.example.practicaltest.spring.domain.product.Product;
+import com.example.practicaltest.spring.domain.product.ProductType;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class OrderProduct {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Product product;
+
+    public OrderProduct(Order order, Product product) {
+        this.order = order;
+        this.product = product;
+    }
+
+}
+
+
+
