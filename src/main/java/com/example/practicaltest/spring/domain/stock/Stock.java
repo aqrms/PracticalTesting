bce807d06c3f666326d4b4a18791d845d691a83e
aqrms <aquarimus@gmail.com>
2024-06-18 14:31:06 +0900

    Add Stock Entity

---
+++ src/main/java/com/example/practicaltest/spring/domain/stock/Stock.java
@@ -0,0 +1,32 @@
+package com.example.practicaltest.spring.domain.stock;
+
+import com.example.practicaltest.spring.domain.BaseEntity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Stock extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String productNumber;
+
+    private int quantity;
+
+    @Builder
+    public Stock(String productNumber, int quantity) {
+        this.productNumber = productNumber;
+        this.quantity = quantity;
+    }
+}
--- src/test/java/com/example/practicaltest/spring/service/order/OrderServiceTest.java
+++ src/test/java/com/example/practicaltest/spring/service/order/OrderServiceTest.java
@@ -6,12 +6,12 @@ import static org.assertj.core.api.Assertions.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.practicaltest.spring.controller.request.OrderCreateRequest;
 import com.example.practicaltest.spring.domain.order.OrderRepository;
@@ -22,6 +22,7 @@ import com.example.practicaltest.spring.domain.product.ProductType;
 import com.example.practicaltest.spring.service.response.OrderResponse;
 
 @ActiveProfiles("test")
+@Transactional
 @SpringBootTest
     // @DataJpaTest
 class OrderServiceTest {
@@ -35,17 +36,19 @@ class OrderServiceTest {
     @Autowired
     private OrderProductRepository orderProductRepository;
 
-    @AfterEach
-    void tearDown() {
-        orderProductRepository.deleteAllInBatch();
-        productRepository.deleteAllInBatch();
-        orderRepository.deleteAllInBatch();
-    }
+    // @AfterEach
+    // void tearDown() {
+    //     orderProductRepository.deleteAllInBatch();
+    //     productRepository.deleteAllInBatch();
+    //     orderRepository.deleteAllInBatch();
+    // }
 
     @DisplayName("주문번호 리스트를 받아 주문을 생성한다.")
     @Test
     public void createOrder() {
         //given
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+
         Product product1 = createProduct(ProductType.HANDMADE, "001", 1000);
         Product product2 = createProduct(ProductType.HANDMADE, "002", 3000);
         Product product3 = createProduct(ProductType.HANDMADE, "003", 5000);
@@ -55,7 +58,38 @@ class OrderServiceTest {
             .productNumbers(List.of("001", "002"))
             .build();
         //when
+        OrderResponse response = orderService.createOrder(request, registeredDateTime);
+
+        //then
+        assertThat(response.getId()).isNotNull();
+        assertThat(response)
+            .extracting("registeredDateTime", "totalPrice")
+            .contains(registeredDateTime, 4000);
+        assertThat(response.getProducts()).hasSize(2)
+            .extracting("productNumber", "price")
+            .containsExactlyInAnyOrder(
+                tuple("001", 1000),
+                tuple("002", 3000)
+            );
+
+    }
+
+    @DisplayName("재고가 있는 상품이 포함된 주문번호 리스트로 주문을 생성한다.")
+    @Test
+    public void createOrderWithStock() {
+        //given
         LocalDateTime registeredDateTime = LocalDateTime.now();
+
+        Product product1 = createProduct(ProductType.HANDMADE, "001", 1000);
+        Product product2 = createProduct(ProductType.HANDMADE, "002", 3000);
+        Product product3 = createProduct(ProductType.HANDMADE, "003", 5000);
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        OrderCreateRequest request = OrderCreateRequest.builder()
+            .productNumbers(List.of("001", "001", "002", "003"))
+            .build();
+
+        //when
         OrderResponse response = orderService.createOrder(request, registeredDateTime);
 
         //then
