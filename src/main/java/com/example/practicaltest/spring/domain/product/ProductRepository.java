57abff57fffd10600377cbc717e94edad9cb2288
aqrms <aquarimus@gmail.com>
2024-06-19 19:24:14 +0900

    Add findLatestProductNumber method and tests

--- src/main/java/com/example/practicaltest/spring/domain/product/ProductRepository.java
+++ src/main/java/com/example/practicaltest/spring/domain/product/ProductRepository.java
@@ -3,11 +3,16 @@ package com.example.practicaltest.spring.domain.product;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     List<Product> findAllBySellingStatusIn(List<ProductSellingStatus> sellingTypes);
+
     List<Product> findAllByProductNumberIn(List<String> productNumbers);
+
+    @Query(value = "select p.product_number from Product p order by id desc limit 1", nativeQuery = true)
+    String findLatestProductNumber();
 }
