2da154ec74399dd0555d699ad5ecbd8474dcfb89
aqrms <aquarimus@gmail.com>
2024-06-19 20:52:15 +0900

    Add createProduct method and tests

---
+++ src/main/java/com/example/practicaltest/spring/service/product/ProductService.java
@@ -0,0 +1,54 @@
+package com.example.practicaltest.spring.service.product;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.practicaltest.spring.controller.product.request.ProductCreateRequest;
+import com.example.practicaltest.spring.domain.product.Product;
+import com.example.practicaltest.spring.domain.product.ProductRepository;
+import com.example.practicaltest.spring.domain.product.ProductSellingStatus;
+import com.example.practicaltest.spring.domain.response.ProductResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductResponse createProduct(ProductCreateRequest request) {
+        String nextProductNumber = createNextProductNumber();
+
+        return ProductResponse.builder()
+            .productNumber(nextProductNumber)
+            .type(request.getType())
+            .sellingStatus(request.getSellingStatus())
+            .name(request.getName())
+            .price(request.getPrice())
+            .build();
+    }
+
+    public List<ProductResponse> getSellingProducts() {
+        List<Product> products = productRepository.findAllBySellingStatusIn(ProductSellingStatus.forDisplay());
+
+        return products.stream()
+            .map(ProductResponse::of)
+            .toList();
+
+    }
+
+    private String createNextProductNumber() {
+        String latestProductNumber = productRepository.findLatestProductNumber();
+        if (latestProductNumber == null) {
+            return "001";
+        }
+
+        int latestProductNumberInt = Integer.parseInt(latestProductNumber);
+        int nextProductNumberInt = latestProductNumberInt + 1;
+
+        return String.format("%03d", nextProductNumberInt);
+    }
+
+}
