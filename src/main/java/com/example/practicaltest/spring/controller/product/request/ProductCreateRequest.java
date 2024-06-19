61cc1911358ff59c6208eaa39002ee5dc1109e94
aqrms <aquarimus@gmail.com>
2024-06-19 19:35:50 +0900

    Add dto builder

--- src/main/java/com/example/practicaltest/spring/controller/product/request/ProductCreateRequest.java
+++ src/main/java/com/example/practicaltest/spring/controller/product/request/ProductCreateRequest.java
@@ -3,6 +3,7 @@ package com.example.practicaltest.spring.controller.product.request;
 import com.example.practicaltest.spring.domain.product.ProductSellingStatus;
 import com.example.practicaltest.spring.domain.product.ProductType;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -15,4 +16,11 @@ public class ProductCreateRequest {
     private String name;
     private int price;
 
+    @Builder
+    public ProductCreateRequest(ProductType type, ProductSellingStatus sellingStatus, String name, int price) {
+        this.type = type;
+        this.sellingStatus = sellingStatus;
+        this.name = name;
+        this.price = price;
+    }
 }
