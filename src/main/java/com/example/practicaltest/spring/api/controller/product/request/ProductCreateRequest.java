ad12ac16c66e9da5cc1b595ced214eaf3051b260
aqrms <aquarimus@gmail.com>
2024-06-20 17:57:44 +0900

    Add validation anno

--- src/main/java/com/example/practicaltest/spring/api/controller/product/request/ProductCreateRequest.java
+++ src/main/java/com/example/practicaltest/spring/api/controller/product/request/ProductCreateRequest.java
@@ -1,9 +1,12 @@
-package com.example.practicaltest.spring.controller.product.request;
+package com.example.practicaltest.spring.api.controller.product.request;
 
 import com.example.practicaltest.spring.domain.product.Product;
 import com.example.practicaltest.spring.domain.product.ProductSellingStatus;
 import com.example.practicaltest.spring.domain.product.ProductType;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,9 +15,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ProductCreateRequest {
 
+    @NotNull
     private ProductType type;
+
+    @NotNull
     private ProductSellingStatus sellingStatus;
+
+    @NotBlank
     private String name;
+
+    @Positive
     private int price;
 
     @Builder
