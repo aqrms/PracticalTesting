0ee56092a5ba03b67448a68029c09c76efac46fb
aqrms <aquarimus@gmail.com>
2024-06-20 17:58:00 +0900

    Add ApiResponse

--- src/main/java/com/example/practicaltest/spring/api/controller/product/ProductController.java
+++ src/main/java/com/example/practicaltest/spring/api/controller/product/ProductController.java
@@ -1,16 +1,19 @@
-package com.example.practicaltest.spring.controller.product;
+package com.example.practicaltest.spring.api.controller.product;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.practicaltest.spring.controller.product.request.ProductCreateRequest;
+import com.example.practicaltest.spring.api.ApiResponse;
+import com.example.practicaltest.spring.api.controller.product.request.ProductCreateRequest;
+import com.example.practicaltest.spring.api.service.product.ProductService;
 import com.example.practicaltest.spring.domain.response.ProductResponse;
-import com.example.practicaltest.spring.service.product.ProductService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -20,8 +23,8 @@ public class ProductController {
     private final ProductService productService;
 
     @PostMapping("api/v1/products/new")
-    public void createProduct(@RequestBody ProductCreateRequest request) {
-        productService.createProduct(request);
+    public ApiResponse<ProductResponse> createProduct(@Valid @RequestBody ProductCreateRequest request) {
+        return ApiResponse.of(HttpStatus.OK, productService.createProduct(request));
     }
 
     @GetMapping("api/v1/products/selling")
