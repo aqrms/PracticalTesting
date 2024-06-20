594bbf6152ba5bb548486cb4a07ec58a7d304e3f
aqrms <aquarimus@gmail.com>
2024-06-20 20:20:25 +0900

    Add OrderControllerTest

--- src/main/java/com/example/practicaltest/spring/api/controller/order/OrderController.java
+++ src/main/java/com/example/practicaltest/spring/api/controller/order/OrderController.java
@@ -6,10 +6,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.practicaltest.spring.api.ApiResponse;
 import com.example.practicaltest.spring.api.controller.order.request.OrderCreateRequest;
 import com.example.practicaltest.spring.api.service.order.OrderService;
 import com.example.practicaltest.spring.api.service.response.OrderResponse;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -19,10 +21,10 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping("/api/v1/orders/new")
-    public OrderResponse createOrder(@RequestBody OrderCreateRequest request) {
+    public ApiResponse<OrderResponse> createOrder(@Valid @RequestBody OrderCreateRequest request) {
         LocalDateTime registeredDateTime = LocalDateTime.now();
 
-        return orderService.createOrder(request, registeredDateTime);
+        return ApiResponse.ok(orderService.createOrder(request, registeredDateTime));
     }
 
 }
