132ef6f7adc3ab56c4686b827aa21356fb71e00b
aqrms <aquarimus@gmail.com>
2024-06-28 16:16:42 +0900

    Add MailServiceTest

---
+++ src/test/java/com/example/practicaltest/spring/api/service/mail/MailServiceTest.java
@@ -0,0 +1,36 @@
+package com.example.practicaltest.spring.api.service.mail;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import com.example.practicaltest.spring.client.mail.MailSendClient;
+import com.example.practicaltest.spring.domain.history.mail.MailSendHistoryRepository;
+
+class MailServiceTest {
+
+    @DisplayName("메일 전송 테스트")
+    @Test
+    void sendMail() {
+        //given
+        MailSendClient mailSendClient = Mockito.mock(MailSendClient.class);
+        MailSendHistoryRepository mailSendHistoryRepository = Mockito.mock(MailSendHistoryRepository.class);
+
+        MailService mailService = new MailService(mailSendClient, mailSendHistoryRepository);
+
+        when(mailSendClient.sendEmail(any(String.class), any(String.class), any(String.class), any(String.class)))
+            .thenReturn(true);
+
+        //when
+        boolean result = mailService.sendMail("", "", "", "");
+
+        //then
+        Assertions.assertThat(result).isTrue();
+
+    }
+
+}
