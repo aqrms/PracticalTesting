21ce470848fb9f6113b4e06706e609e9276e2cf0
aqrms <aquarimus@gmail.com>
2024-07-01 22:13:43 +0900

    Refactor Mock dependency

--- src/test/java/com/example/practicaltest/spring/api/service/mail/MailServiceTest.java
+++ src/test/java/com/example/practicaltest/spring/api/service/mail/MailServiceTest.java
@@ -6,23 +6,30 @@ import static org.mockito.Mockito.*;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.example.practicaltest.spring.client.mail.MailSendClient;
 import com.example.practicaltest.spring.domain.history.mail.MailSendHistory;
 import com.example.practicaltest.spring.domain.history.mail.MailSendHistoryRepository;
 
+@ExtendWith(MockitoExtension.class)
 class MailServiceTest {
+
+    @Mock
+    private MailSendClient mailSendClient;
+    @Mock
+    private MailSendHistoryRepository mailSendHistoryRepository;
+    @InjectMocks
+    private MailService mailService;
 
     @DisplayName("메일 전송 테스트")
     @Test
     void sendMail() {
         //given
-        MailSendClient mailSendClient = Mockito.mock(MailSendClient.class);
-        MailSendHistoryRepository mailSendHistoryRepository = Mockito.mock(MailSendHistoryRepository.class);
-
-        MailService mailService = new MailService(mailSendClient, mailSendHistoryRepository);
-
         when(mailSendClient.sendEmail(any(String.class), any(String.class), any(String.class), any(String.class)))
             .thenReturn(true);
 
